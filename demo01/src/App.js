0cb130bd4f143bb3a66cb8683a2fac1796df6b84
Janne Turunen <janne.turunen@xamk.fi>
2021-01-12 11:48:28 +0200

    React-perusteita 1

--- demo01/src/App.js
+++ demo01/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [tervehdys] = useState("Moikka!"); 
+  
   return (
-    <div className="App">
-      Heippa!
+    <div className="container">
+      
+      <h1>Demo 1: React-perusteita</h1>
+
+      <h2>"Hello world"</h2>
+
+      <button className="btn btn-primary btn-block">Sano heippa!</button>
+
+      {tervehdys}
+
     </div>
   );
+
 }
 
 export default App;
