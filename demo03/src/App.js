7aeeb5942728b444a2d94971da306381064e75ad
Janne Turunen <janne.turunen@xamk.fi>
2021-01-26 11:40:12 +0200

    Tietojen hakeminen palvelimelta

--- demo03/src/App.js
+++ demo03/src/App.js
@@ -2,14 +2,35 @@ import { useState, useEffect } from "react";
 
 function App() {
 
-  const [henkilot, setHenkilot] = useState([]);
+  const [data, setData] = useState({
+                                      henkilot : [],
+                                      tiedotHaettu : false,
+                                      virhe : null
+                                   });
 
   const haeTiedot = async () => {
 
-    const yhteys = await fetch("https://jsonplaceholder.typicode.com/users");
-    const tiedot = await yhteys.json();
+    try {
 
-    setHenkilot(tiedot);
+      const yhteys = await fetch("https://jsonplaceholder.typicode.com/users");
+      const tiedot = await yhteys.json();
+  
+      setData({
+                ...data,
+                henkilot : tiedot,
+                tiedotHaettu : true
+              });
+
+    } catch (e) {
+
+      setData({
+        ...data,
+        tiedotHaettu : false,
+        virhe : "Yhteyttä palvelimeen ei voitu muodostaa. Yritä hetken kuluttua uudestaan."
+      });
+
+
+    }
 
   }
 
@@ -24,8 +45,27 @@ function App() {
 
       <h1>Demo 3: Tietojen hakeminen palvelimelta</h1>
 
-      {JSON.stringify(henkilot)}
+      <ul className="list-group">
 
+      {(!data.virhe)
+        ? (data.tiedotHaettu)
+          ? data.henkilot.map((henkilo, idx) => {
+
+                                                  return (
+                                                          <li className="list-group-item" key={idx}>
+                                                            <h5>{henkilo.name}</h5>
+                                                            {henkilo.email}
+                                                          </li>
+                                                        )
+
+            })
+          : <p>Tietoja haetaan... odota hetki.</p>
+        : <div className="alert alert-danger">
+            {data.virhe}
+          </div>
+      }
+
+      </ul>
     </div>
   );
 }
